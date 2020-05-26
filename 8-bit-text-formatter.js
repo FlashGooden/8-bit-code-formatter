33560b2ab81b4d0a396aad1e24953f6fa01b0f07
Gamerman2001 <Tro12185@gmail.com>
2020-05-26 12:15:34 -0400

    made map

---
+++ 8-bit-text-formatter.js
@@ -0,0 +1,16 @@
+//we will create a text formatter file that will take in a string with a length no longer than 4  and output an encoded value representing that string
+
+//our encoder has 32 slots in which we will map over a value to which position it will be encoded to
+
+//create an object that takes each character then has a key for the original slot, then places the value in the encoding slot
+
+let encodedMap = {
+  charOne: { 0: 3, 1: 7, 2: 11, 3: 15, 4: 19, 5: 23, 6: 27, 7: 31 },
+  charTwo: { 0: 2, 1: 6, 2: 10, 3: 14, 4: 18, 5: 22, 6: 26, 7: 30 },
+  charThree: { 0: 1, 1: 5, 2: 9, 3: 13, 4: 17, 5: 21, 6: 25, 7: 29 },
+  charFour: { 0: 0, 1: 4, 2: 8, 3: 12, 4: 16, 5: 20, 6: 24, 7: 28 },
+};
+
+for (x in encodedMap.charOne) {
+  console.log(encodedMap.charOne[x]);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 8-bit-code-formatter
+
+text formatter file that will take in a string with a length no longer than 4  and output an encoded value representing that string
